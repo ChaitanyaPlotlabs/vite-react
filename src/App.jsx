294412bbabf1c7b2771f2823584c7636d7994e49
ChhikaraBRUH <chaitanyachhikara@gmail.com>
2023-12-06 10:59:39 +0530

    feat: add tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+    <div className="bg-gray-900 w-full min-h-[100vh] items-center justify-center flex flex-col gap-10">
+      <div className="flex flex-row items-center justify-center gap-4">
+        <a href="https://vitejs.dev">
+          <img src={viteLogo} className="h-32 w-auto" alt="Vite logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+        <a href="https://react.dev">
+          <img src={reactLogo} className="h-32 w-auto" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+      <h1 className="text-4xl font-bold text-white">Vite + React</h1>
+
+      <button
+        className="bg-black border-2 border-black hover:border-blue-500 transition-all text-white font-bold py-2 px-4 rounded"
+        onClick={() => setCount((count) => count + 1)}
+      >
+        count is {count}
+      </button>
+    </div>
+  );
 }
 
-export default App
+export default App;
